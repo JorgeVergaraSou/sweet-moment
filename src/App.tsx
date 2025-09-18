d5e424c55d8774d5d85d69458644a2f201a0527a
jorge vergara <jorgevergarasou@gmail.com>
2025-09-18 12:18:04 -0300

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 //src/App.tsx
-import { BrowserRouter, Navigate, Route } from "react-router-dom";
+import { HashRouter, Navigate, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
@@ -20,40 +20,117 @@ const Login = lazy(() => import("./pages/Login/Login"));
 const Private = lazy(() => import("./pages/Private/Private"));
 const Home = lazy(() => import("./pages/Public/Home"));
 
-export default function App() {
-  const basename = process.env.NODE_ENV === "production" ? "/sweet-moment-candy" : "/";
+function AppContent() {
+  return (
+    <RoutesWithNotFound>
+      {/* Rutas Home sin header/footer */}
+      <Route
+        path={PublicRoutes.HOME}
+        element={
+          <HomeLayout>
+            <Home />
+          </HomeLayout>
+        }
+      />
 
+      {/* Rutas públicas normales */}
+      <Route
+        path={PublicRoutes.SERVICIOS}
+        element={
+          <MainLayout>
+            <Servicios />
+          </MainLayout>
+        }
+      />
+      <Route
+        path={PublicRoutes.CONTACTO}
+        element={
+          <MainLayout>
+            <ContactoPage />
+          </MainLayout>
+        }
+      />
+      <Route
+        path={PublicRoutes.GALERIA}
+        element={
+          <MainLayout>
+            <GaleriaPage />
+          </MainLayout>
+        }
+      />
+      <Route
+        path={PublicRoutes.LOGIN}
+        element={
+          <MainLayout>
+            <Login />
+          </MainLayout>
+        }
+      />
+
+      {/* Rutas privadas */}
+      <Route element={<AuthGuard privateValidation={true} />}>
+        <Route
+          path={`${PrivateRoutes.PRIVATE}/*`}
+          element={
+            <MainLayout>
+              <Private />
+            </MainLayout>
+          }
+        />
+
+        <Route
+          path={PrivateRoutes.USER}
+          element={
+            <MainLayout>
+              <div>Usuario</div>
+            </MainLayout>
+          }
+        />
+        <Route
+          path={PrivateRoutes.PERFIL}
+          element={
+            <MainLayout>
+              <div>Perfil</div>
+            </MainLayout>
+          }
+        />
+
+        {/* Rutas solo admin */}
+        <Route element={<RoleGuard role={Roles.ADMIN} />}>
+          <Route
+            path={PrivateRoutes.ADMIN}
+            element={
+              <MainLayout>
+                <Admin />
+              </MainLayout>
+            }
+          />
+        </Route>
+
+        <Route
+          path={PrivateRoutes.LOGOUT}
+          element={<Navigate replace to={PublicRoutes.LOGIN} />}
+        />
+      </Route>
+
+      {/* Redirección raíz */}
+      <Route path="/" element={<Navigate replace to={PublicRoutes.HOME} />} />
+    </RoutesWithNotFound>
+  );
+}
+
+export default function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Provider store={store}>
-        <BrowserRouter basename={basename}>
-          <RoutesWithNotFound>
-            {/* Redirección raíz */}
-            <Route path="/" element={<Navigate replace to={PublicRoutes.HOME} />} />
-
-            {/* Rutas públicas */}
-            <Route path={PublicRoutes.HOME} element={<HomeLayout><Home /></HomeLayout>} />
-            <Route path={PublicRoutes.SERVICIOS} element={<MainLayout><Servicios /></MainLayout>} />
-            <Route path={PublicRoutes.CONTACTO} element={<MainLayout><ContactoPage /></MainLayout>} />
-            <Route path={PublicRoutes.GALERIA} element={<MainLayout><GaleriaPage /></MainLayout>} />
-            <Route path={PublicRoutes.LOGIN} element={<MainLayout><Login /></MainLayout>} />
-
-            {/* Rutas privadas */}
-            <Route element={<AuthGuard privateValidation={true} />}>
-              <Route path={`${PrivateRoutes.PRIVATE}/*`} element={<MainLayout><Private /></MainLayout>} />
-              <Route path={PrivateRoutes.USER} element={<MainLayout><div>Usuario</div></MainLayout>} />
-              <Route path={PrivateRoutes.PERFIL} element={<MainLayout><div>Perfil</div></MainLayout>} />
-              <Route element={<RoleGuard role={Roles.ADMIN} />}>
-                <Route path={PrivateRoutes.ADMIN} element={<MainLayout><Admin /></MainLayout>} />
-              </Route>
-              <Route path={PrivateRoutes.LOGOUT} element={<Navigate replace to={PublicRoutes.LOGIN} />} />
-            </Route>
-          </RoutesWithNotFound>
-        </BrowserRouter>
+        <HashRouter>
+          <AppContent />
+        </HashRouter>
       </Provider>
     </Suspense>
   );
 }
+
 
 
 /*
