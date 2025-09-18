104adbfe94c17dca18fc6317531c37f596e36353
jorge vergara <jorgevergarasou@gmail.com>
2025-09-18 08:56:38 -0300

    errores antes de build arreglados

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,4 @@ export * from './Footer';
 export * from './Galeria/Galeria';
 export * from './Header';
 export * from './Logout/Logout';
-export * from './NavBars/DropdownMenu';
 export * from './WhatsAppButton';
